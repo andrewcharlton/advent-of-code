016da9d1bb2c87e7165907f8a0a33238c8bbb066
Andrew Charlton <andy@timescale.com>
2024-12-01 08:45:00 +0000

    Add template and scaffolding for 2024

---
+++ 2024/day03/src/main.rs
@@ -0,0 +1,29 @@
+const INPUT: &str = include_str!("../input.txt");
+
+fn main() {
+    println!("Part one: {}", part_one(INPUT));
+    println!("Part two: {}", part_two(INPUT));
+}
+
+fn part_one(input: &str) -> u64 {
+    0
+}
+
+
+fn part_two(input: &str) -> u64 {
+    0
+}
+
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    const EXAMPLE: &str = include_str!("../example.txt");
+
+    #[test]
+    fn example() {
+        assert_eq!(part_one(EXAMPLE), 0);
+        assert_eq!(part_two(EXAMPLE), 0);
+    }
+}
