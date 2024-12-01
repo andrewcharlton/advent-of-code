4d1101ce0b4f8c6b41ec980b6ae2a3a33caae9f2
Andrew Charlton <andy@timescale.com>
2024-12-01 09:02:43 +0000

    2024 Day01

--- 2024/day01/src/main.rs
+++ 2024/day01/src/main.rs
@@ -1,3 +1,5 @@
+use std::collections::HashMap;
+
 const INPUT: &str = include_str!("../input.txt");
 
 fn main() {
@@ -6,14 +8,43 @@ fn main() {
 }
 
 fn part_one(input: &str) -> u64 {
-    0
-}
+    let (mut left, mut right) = parse_input(input);
+    left.sort();
+    right.sort();
 
+    left.iter()
+        .zip(right.iter())
+        .map(|(a, b)| a.abs_diff(*b))
+        .sum()
+}
 
 fn part_two(input: &str) -> u64 {
-    0
+    let (mut left, right) = parse_input(input);
+    left.sort();
+
+    let mut counts: HashMap<u64, u64> = HashMap::new();
+    for x in right {
+        if let Some(v) = counts.get_mut(&x) {
+            *v += 1;
+        } else {
+            counts.insert(x, 1);
+        }
+    }
+
+    left.iter().map(|x| x * counts.get(&x).unwrap_or(&0)).sum()
 }
 
+fn parse_input(input: &str) -> (Vec<u64>, Vec<u64>) {
+    input
+        .lines()
+        .map(|line| {
+            let mut parts = line.split_whitespace();
+            let left = parts.next().unwrap().parse::<u64>().unwrap();
+            let right = parts.next().unwrap().parse::<u64>().unwrap();
+            (left, right)
+        })
+        .unzip()
+}
 
 #[cfg(test)]
 mod test {
@@ -23,7 +54,7 @@ mod test {
 
     #[test]
     fn example() {
-        assert_eq!(part_one(EXAMPLE), 0);
-        assert_eq!(part_two(EXAMPLE), 0);
+        assert_eq!(part_one(EXAMPLE), 11);
+        assert_eq!(part_two(EXAMPLE), 31);
     }
 }
