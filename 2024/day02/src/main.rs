59d6a2f6fd980ed53ffe59508dbf5a4639478c0d
Andrew Charlton <andy@timescale.com>
2024-12-02 10:10:56 +0000

    Day02 brute force

--- 2024/day02/src/main.rs
+++ 2024/day02/src/main.rs
@@ -1,3 +1,6 @@
+use itertools::Itertools;
+use std::cmp;
+
 const INPUT: &str = include_str!("../input.txt");
 
 fn main() {
@@ -5,14 +8,56 @@ fn main() {
     println!("Part two: {}", part_two(INPUT));
 }
 
-fn part_one(input: &str) -> u64 {
-    0
+fn part_one(input: &str) -> usize {
+    input.lines()
+        .filter_map(|line| {
+            let nums: Vec<i64> = line.split_whitespace().map(|n| n.parse::<i64>().unwrap()).collect();
+            if is_safe(&nums) { Some(()) } else { None }
+        })
+    .count()
 }
 
 
-fn part_two(input: &str) -> u64 {
-    0
+fn part_two(input: &str) -> usize {
+    input.lines()
+        .filter_map(|line| {
+            let nums: Vec<i64> = line.split_whitespace().map(|n| n.parse::<i64>().unwrap()).collect();
+            if is_safe(&nums) {
+                return Some(());
+            }
+
+            for i in 0..nums.len() {
+                let mut reduced_nums = nums.clone();
+                reduced_nums.remove(i);
+                if is_safe(&reduced_nums) {
+                    return Some(());
+                }
+            }
+
+            None
+        })
+    .count()
 }
+
+fn is_safe(nums: &Vec<i64>) -> bool {
+    let mut up = 0;
+    let mut down = 0;
+    for (a, b) in nums.into_iter().tuple_windows() {
+        let diff = a.abs_diff(*b);
+        if diff == 0 || diff > 3 {
+            return false;
+        }
+
+        if a > b {
+            down += 1;
+        } else if a < b {
+            up += 1;
+        }
+    }
+
+    return up == 0 || down == 0
+}
+
 
 
 #[cfg(test)]
@@ -23,7 +68,7 @@ mod test {
 
     #[test]
     fn example() {
-        assert_eq!(part_one(EXAMPLE), 0);
-        assert_eq!(part_two(EXAMPLE), 0);
+        assert_eq!(part_one(EXAMPLE), 2);
+        assert_eq!(part_two(EXAMPLE), 4);
     }
 }
