5b72276cfd7125da76cbe4bbc6b84bce79e26614
Andrew Charlton <andy@timescale.com>
2024-12-03 08:15:14 +0000

    Fix template

--- 2024/day13/src/main.rs
+++ 2024/day13/src/main.rs
@@ -1,16 +1,23 @@
+use std::time::Instant;
+
 const INPUT: &str = include_str!("../input.txt");
 
 fn main() {
+    let now = Instant::now();
+
     println!("Part one: {}", part_one(INPUT));
     println!("Part two: {}", part_two(INPUT));
+
+    let elapsed_time = now.elapsed();
+    println!("Solved in {}µs", elapsed_time.as_micros());
 }
 
-fn part_one(input: &str) -> u64 {
+fn part_one(input: &str) -> i64 {
     0
 }
 
 
-fn part_two(input: &str) -> u64 {
+fn part_two(input: &str) -> i64 {
     0
 }
 
