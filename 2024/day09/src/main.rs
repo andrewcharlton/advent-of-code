b1b60cbc01b639debec3513d5ff84ee96a2c7089
Andrew Charlton <andy@timescale.com>
2024-12-18 07:01:07 +0000

    2024 Day 09

--- 2024/day09/src/main.rs
+++ 2024/day09/src/main.rs
@@ -5,21 +5,98 @@ const INPUT: &str = include_str!("../input.txt");
 fn main() {
     let now = Instant::now();
 
-    println!("Part one: {}", part_one(INPUT));
-    println!("Part two: {}", part_two(INPUT));
+    println!("Part one: {}", solve(INPUT, true));
+    println!("Part two: {}", solve(INPUT, false));
 
     let elapsed_time = now.elapsed();
     println!("Solved in {}µs", elapsed_time.as_micros());
 }
 
-fn part_one(input: &str) -> i64 {
-    0
+fn solve(input: &str, single_blocks: bool) -> usize {
+    let mut disk = Disk::new(input, single_blocks);
+    disk.solve()
+}
+
+struct Disk {
+    blocks: Vec<Block>,
+    gaps: Vec<Gap>,
+}
+
+impl Disk {
+    fn new(input: &str, single_blocks: bool) -> Self {
+        let mut id = 0;
+        let mut pos = 0;
+        let mut blocks: Vec<Block> = Vec::new();
+        let mut gaps: Vec<Gap> = Vec::new();
+
+        for (i, c) in input.trim().chars().enumerate() {
+            let n: usize = c.to_digit(10).unwrap().try_into().unwrap();
+            if n == 0 {
+                continue;
+            }
+            if i % 2 == 0 {
+                if single_blocks {
+                    for p in pos..pos+n {
+                        blocks.push(Block{pos: p, id, len: 1});
+                    }
+                } else {
+                    blocks.push(Block{pos, id, len: n});
+                }
+
+                id += 1;
+            } else {
+                gaps.push(Gap{pos, len: n});
+            }
+
+            pos += n;
+        }
+
+        Disk{blocks, gaps}
+    }
+
+    fn solve(&mut self) -> usize {
+        for i in (0..self.blocks.len()).rev() {
+            let block = self.blocks.get_mut(i).unwrap();
+            for gap in self.gaps.iter_mut() {
+                if gap.pos > block.pos {
+                    continue;
+                }
+
+                if gap.len >= block.len {
+                    block.pos = gap.pos;
+                    gap.pos += block.len;
+                    gap.len -= block.len;
+
+                    break;
+                }
+            }
+        }
+
+        self.blocks.iter().map(|block| block.checksum()).sum()
+    }
+}
+
+#[derive(Debug,Clone)]
+struct Block {
+    pos: usize,
+    id: usize,
+    len: usize,
+}
+
+impl Block {
+    fn checksum(&self) -> usize {
+        (self.pos..self.pos+self.len).map(|i| i*self.id).sum()
+    }
+}
+
+#[derive(Debug)]
+struct Gap {
+    pos: usize,
+    len: usize,
 }
 
 
-fn part_two(input: &str) -> i64 {
-    0
-}
+
 
 
 #[cfg(test)]
@@ -30,7 +107,7 @@ mod test {
 
     #[test]
     fn example() {
-        assert_eq!(part_one(EXAMPLE), 0);
-        assert_eq!(part_two(EXAMPLE), 0);
+        assert_eq!(solve(EXAMPLE, true), 1928);
+        assert_eq!(solve(EXAMPLE, false), 2858);
     }
 }
