dc08edd9bd820935faa00cf670867b838ce0af38
Andrew Charlton <andy@timescale.com>
2024-12-30 21:14:44 +0000

    2024 Day 23

--- 2024/day23/src/main.rs
+++ 2024/day23/src/main.rs
@@ -1,3 +1,5 @@
+use itertools::Itertools;
+use std::collections::HashSet;
 use std::time::Instant;
 
 const INPUT: &str = include_str!("../input.txt");
@@ -5,22 +7,132 @@ const INPUT: &str = include_str!("../input.txt");
 fn main() {
     let now = Instant::now();
 
-    println!("Part one: {}", part_one(INPUT));
-    println!("Part two: {}", part_two(INPUT));
+    let network = Network::new(INPUT);
+    println!("Part one: {}", network.triad_count());
+    println!("Part two: {}", network.password());
 
     let elapsed_time = now.elapsed();
     println!("Solved in {}µs", elapsed_time.as_micros());
 }
 
-fn part_one(input: &str) -> i64 {
-    0
+type Computer = [char; 2];
+
+struct Network {
+    connections: HashSet<(Computer, Computer)>,
 }
 
+impl Network {
+    fn new(input: &str) -> Self {
+        let connections: HashSet<(Computer, Computer)> = input
+            .lines()
+            .map(|line| {
+                let (a, b) = line.split_once("-").unwrap();
 
-fn part_two(input: &str) -> i64 {
-    0
+                let mut a = a.chars();
+                let a: Computer = [a.next().unwrap(), a.next().unwrap()];
+
+                let mut b = b.chars();
+                let b: Computer = [b.next().unwrap(), b.next().unwrap()];
+
+                if a < b {
+                    (a, b)
+                } else {
+                    (b, a)
+                }
+            })
+            .collect();
+
+        Network { connections }
+    }
+
+    fn triad_count(&self) -> usize {
+        let groups = self.initial_groups();
+        let mut triads = self.group(&groups);
+        triads.sort();
+
+        triads
+            .iter()
+            .filter(|triad| triad.len() == 3)
+            .filter_map(|triad| triad.iter().find(|computer| computer[0] == 't'))
+            .count()
+    }
+
+    fn password(&self) -> String {
+        let mut groups = self.initial_groups();
+        let mut best = groups.get(0).unwrap().clone();
+
+        loop {
+            println!("Looping - Groups: {}, Best: {:?}", groups.len(), best);
+            groups = self.group(&groups);
+            if groups.len() == 0 {
+                return best
+                    .iter()
+                    .map(|c| {
+                        let mut s = String::new();
+                        s.push(c[0]);
+                        s.push(c[1]);
+                        s
+                    })
+                    .join(",");
+            }
+
+            let max_len = groups.iter().map(|g| g.len()).max().unwrap();
+            groups = groups.into_iter().filter(|g| g.len() == max_len).collect();
+
+            for g in &groups {
+                if g.len() > best.len() {
+                    best = g.to_vec();
+                }
+            }
+        }
+    }
+
+    fn group(&self, groups: &Vec<Vec<Computer>>) -> Vec<Vec<Computer>> {
+        let mut results = Vec::new();
+
+        for i in 0..groups.len() - 1 {
+            let a = groups.get(i).unwrap();
+
+            for j in i + 1..groups.len() {
+                let b = groups.get(j).unwrap();
+                if let Some(new_group) = self.union(a, b) {
+                    results.push(new_group);
+                }
+            }
+        }
+
+        results.sort();
+        results.dedup();
+        results
+    }
+
+    fn union(&self, a: &Vec<Computer>, b: &Vec<Computer>) -> Option<Vec<Computer>> {
+        let mut union = a.clone();
+        union.extend(b);
+        union.sort();
+        union.dedup();
+
+        for i in 0..union.len() - 1 {
+            let x = *union.get(i).unwrap();
+            for j in i + 1..union.len() {
+                let y = *union.get(j).unwrap();
+                if !self.connections.contains(&(x, y)) {
+                    return None;
+                }
+            }
+        }
+
+        Some(union)
+    }
+
+    fn initial_groups(&self) -> Vec<Vec<Computer>> {
+        self.connections
+            .clone()
+            .into_iter()
+            .map(|(a, b)| vec![a, b])
+            .collect()
+    }
 }
-
 
 #[cfg(test)]
 mod test {
@@ -30,7 +142,8 @@ mod test {
 
     #[test]
     fn example() {
-        assert_eq!(part_one(EXAMPLE), 0);
-        assert_eq!(part_two(EXAMPLE), 0);
+        let network = Network::new(EXAMPLE);
+        assert_eq!(network.triad_count(), 7);
+        assert_eq!(network.password(), String::from("co,de,ka,ta"));
     }
 }
